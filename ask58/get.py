661ee6e6cff11eb3f0d2844c0624d4d5587a6817
changji2069 <33982721+changji2069@users.noreply.github.com>
2017-11-25 19:43:25 +0000

    Update get.py

--- ask58/get.py
+++ ask58/get.py
@@ -1,13 +1,13 @@
-import requests
-r = requests.get('some url')
-print r.json() # if response type is already in json, will automatically get json
-
-#or 
-
 ### import urllib.request if it doesn't work
 
 import urllib.request, json 
-with urllib.request.urlopen(" #server url ") as url:
-    data = json.loads(url.read().decode())
-    print(data)
+response = urllib.request.urlopen('#server url')
+data = json.loads(response)
+print(data)
+    
+# or 
+
+import requests
+response = requests.get('some url')
+print response.json() # if response type is already in json, will automatically get json
     
